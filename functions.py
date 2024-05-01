0d080404e170a76860e30c452fc3d2565e3e4eb9
nitrolanco <mamibalss@gmail.com>
2024-04-30 22:28:26 -0400

    Test 1 approved
    Test 1 approved

--- functions.py
+++ functions.py
@@ -1,3 +1,11 @@
+import datetime
+
 def welcome(name):
     
-    return
+    current_hour = datetime.datetime.now().hour
+    if 20 <= current_hour < 24 or 0 <= current_hour < 6:
+        return f'¡Buenas noches {name}!'
+    elif 6 <= current_hour < 12:
+        return f'¡Buenos días {name}!'
+    elif 12 <= current_hour < 20:
+        return f'¡Buenas tardes {name}!'
